536b14821d4d1afd24af83ab841a05ec16d76b7a
Liam <27wlessley@student.harrisburg.k12.or.us>
2024-09-24 15:41:21 -0700

    ll
    l

--- TeamCode/RaidersUSEUSEUSE.java
+++ TeamCode/RaidersUSEUSEUSE.java
@@ -1,2 +1,16 @@
-public class RaidersUSEUSEUSE {
-}
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+public class RaidersUSEUSEUSE extends LinearOpMode {
+
+private DcMotor motor1leftmotor;
+private DcMotor motor0rightmotor;
+private DcMotor motor2botright;
+private DcMotor motor3botleft;
+
+@Override
+public void runOpMode() {
+motor1leftmotor = hardwareMap.get(DcMotor.class, "motor 1/left motor"
